ca6b8e3bd7a19cb9d6dea24c2bfb1230f8c8d082
donatselmanii <124844058+donatselmanii@users.noreply.github.com>
2023-04-28 15:56:04 +0200

    Ndarja e folderave te front end dhe backend dhe lidhja e tyre

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -5,6 +5,7 @@ import Wrapper from './homepagecomponents/Wrapper';
 import AboutUs from './homepagecomponents/AboutUs';
 import LearnMore from './homepagecomponents/LearnMore';
 import Footer from './homepagecomponents/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function Homepage() {
   return (
       <> 
